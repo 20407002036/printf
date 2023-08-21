0843ea599c4740d967bb81540a4baf04e8811711
jcode109tech <jedydiahr9@gmail.com>
2023-08-22 00:35:18 +0300

    commit for printf

--- _printf.c
+++ _printf.c
@@ -8,85 +8,87 @@ int buffer_print(char buffer);
 int handle_character(const char *format, va_list args);
 
 /**
-* _printf - imitates the simple actions of printf
-* @format: pointer to the string to be printed
-* Return: int
-*/
+ * _printf - imitates the simple actions of printf
+ * @format: pointer to the string to be printed
+ * Return: int
+ */
 
 int _printf(const char *format, ...)
 {
-int count = 0;
+	int count = 0;
+	va_list args;
+	va_start(args, format);
 
-va_list args;
-va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-if (format == NULL)
-return (-1);
-else
-count = handle_character(format, args);
+	count = handle_character(format, args);
 
-va_end(args);
-return (count);
+	va_end(args);
+
+	return (count);
 }
 /**
-* handle_character - changes formart spefiers
-* @format: string to run
-* @args: arguments
-* Return: int
-*/
+ * handle_character - changes formart spefiers
+ * @format: string to run
+ * @args: arguments
+ * Return: int
+ */
 
 int handle_character(const char *format, va_list args)
 {
-int count = 0;
-const char *ptr = format;
+	int count = 0;
+	const char *ptr = format;
 
-while (*ptr != '\0')
-{
-if (*ptr == '%')
-{
-ptr++;
-if (*ptr == 's')
-{
-const char *str = va_arg(args, const char *);
-while (*str != '\0')
-{
-count += buffer_print(*str);
-str++;
-}
-}
-else if (*ptr == 'c')
-{
-char c = va_arg(args, int);
-count += buffer_print(c);
-}
-else if (*ptr == '%')
-{
-count += buffer_print('%');
-}
-else
-{
-count += buffer_print(*ptr);
-}
-ptr++;
-}
-else
-{
-count += buffer_print(*ptr);
-ptr++;
-}
-}
-return (count);
+	while (*ptr != '\0')
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			if (*ptr == 's')
+			{
+				const char *str = va_arg(args, const char *);
+
+				while (*str != '\0')
+				{
+					count += buffer_print(*str);
+					str++;
+				}
+			}
+			else if (*ptr == 'c')
+			{
+				char c = va_arg(args, int);
+
+				count += buffer_print(c);
+			}
+			else if (*ptr == '%')
+			{
+				count += buffer_print('%');
+			}
+			else
+			{
+				count += buffer_print(*ptr);
+			}
+			ptr++;
+		}
+		else
+		{
+			count += buffer_print(*ptr);
+			ptr++;
+		}
+	}
+	return (count);
 }
 
 
 /**
-* buffer_print - prints the contents of the string
-* @buffer: char to be printed
-* Return: int
-*/
+ * buffer_print - prints the contents of the string
+ * @buffer: char to be printed
+ * Return: int
+ */
 int buffer_print(char buffer)
 {
-return (write(1, &buffer, 1));
+	return (write(1, &buffer, 1));
 }
 
 
