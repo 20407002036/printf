aab2c83cf8dd64f11b8ae1df0f372685e940b0f3
jcode109tech <jedydiahr9@gmail.com>
2023-08-22 00:08:48 +0300

    Commit for printf

--- _printf.c
+++ _printf.c
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+
+int buffer_print(char buffer);
+int handle_character(const char *format, va_list args);
+
 /**
 * _printf - imitates the simple actions of printf
 * @format: pointer to the string to be printed
@@ -11,21 +15,79 @@
 
 int _printf(const char *format, ...)
 {
-int count = 0;
+    int count = 0;
 
-va_list args;
-va_start(args, format);
+    va_list args;
+    va_start(args, format);
 
-if (format == NULL)
-return (-1);
-else
-handle_character(format, args);
+    if (format == NULL)
+        return (-1);
+    else
+        count = handle_character(format, args); 
 
-va_end(args);
-return (count);
+    va_end(args);
+    return (count);
+}
+/**
+* handle_character - changes formart spefiers
+* @formart: string to run
+* @args: arguments
+* Return: int
+*/
+
+int handle_character(const char *format, va_list args)
+{
+    int count = 0;
+    const char *ptr = format;
+
+    while (*ptr != '\0')
+    {
+        if (*ptr == '%')
+        {
+            ptr++;
+            if (*ptr == 's')
+            {
+                const char *str = va_arg(args, const char *);
+                while (*str != '\0')
+                {
+                    count += buffer_print(*str);
+                    str++;
+                }
+            }
+            else if (*ptr == 'c')
+            {
+                char c = va_arg(args, int);
+                count += buffer_print(c);
+            }
+            else if (*ptr == '%')
+            {
+                count += buffer_print('%');
+            }
+            else
+            {
+                count += buffer_print(*ptr);
+            }
+            ptr++;
+        }
+        else
+        {
+            count += buffer_print(*ptr);
+            ptr++;
+        }
+    }
+    return (count);
 }
 
 
+/**
+* buffer_print - prints the contents of the string
+* @buffer: char to be printed
+* Return: int
+*/
+int buffer_print(char buffer)
+{
+    return (write(1, &buffer, 1));
+}
 
 
 
--- main.h
+++ main.h
@@ -3,7 +3,5 @@
 #include<stdarg.h>
 
 int _printf(const char *format, ...);
-int buffer_print(char buffer);
-int handle_character(const char *formart, va_list args);
 
 #endif 
